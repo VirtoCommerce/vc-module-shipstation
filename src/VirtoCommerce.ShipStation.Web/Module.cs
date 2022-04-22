8e84c16cb7df140b1b07ddab57f04fe5f1021874
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-04-22 19:11:35 +0400

    Remove db context

--- src/VirtoCommerce.ShipStation.Web/Module.cs
+++ src/VirtoCommerce.ShipStation.Web/Module.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
@@ -8,7 +7,6 @@ using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.ShipStationModule.Core;
 using VirtoCommerce.ShipStationModule.Core.Services;
-using VirtoCommerce.ShipStationModule.Data.Repositories;
 using VirtoCommerce.ShipStationModule.Data.Services;
 
 namespace VirtoCommerce.ShipStationModule.Web;
@@ -20,13 +18,6 @@ public class Module : IModule, IHasConfiguration
 
     public void Initialize(IServiceCollection serviceCollection)
     {
-        // database initialization
-        serviceCollection.AddDbContext<ShipStationDbContext>((provider, options) =>
-        {
-            var configuration = provider.GetRequiredService<IConfiguration>();
-            options.UseSqlServer(configuration.GetConnectionString(ModuleInfo.Id) ?? configuration.GetConnectionString("VirtoCommerce"));
-        });
-
         serviceCollection.AddTransient<IShipStationService, ShipStationService>();
     }
 
@@ -45,16 +36,6 @@ public class Module : IModule, IHasConfiguration
                 ModuleId = ModuleInfo.Id,
                 Name = x
             }).ToArray());
-
-        // Ensure that any pending migrations are applied
-        using (var serviceScope = appBuilder.ApplicationServices.CreateScope())
-        {
-            using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<ShipStationDbContext>())
-            {
-                dbContext.Database.EnsureCreated();
-                dbContext.Database.Migrate();
-            }
-        }
     }
 
     public void Uninstall()
